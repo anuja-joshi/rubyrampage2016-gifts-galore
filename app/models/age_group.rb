9f59c88223d468780e46377899a35305e9a85ac1
Shweta <shweta@joshsoftware.com>
2016-10-22 18:59:01 +0530

    Added dependent destroy to models

--- app/models/age_group.rb
+++ app/models/age_group.rb
@@ -1,5 +1,5 @@
 class AgeGroup < ApplicationRecord
   validates :gender, inclusion: { in: %w(f m), message: "Invalid gender!!" }
 
-  has_many :categories
+  has_many :categories, dependent: :destroy
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates :gender, inclusion: { in: %w(f m), message: "Invalid gender!!" }
 
   belongs_to :age_group
-  has_many :gifts
+  has_many :gifts, dependent: :destroy
 end
