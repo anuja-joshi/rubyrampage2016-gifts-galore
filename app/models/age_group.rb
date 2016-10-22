a13d8a8ae59767228f08ab3de35a31e100b06b42
Shweta <shweta@joshsoftware.com>
2016-10-22 18:05:16 +0530

    added assoication in model

--- app/models/age_group.rb
+++ app/models/age_group.rb
@@ -1,3 +1,5 @@
 class AgeGroup < ApplicationRecord
   validates :gender, inclusion: { in: %w(f m), message: "Invalid gender!!" }
+
+  has_many :categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   validates :gender, inclusion: { in: %w(f m), message: "Invalid gender!!" }
+
+  belongs_to :age_group
 end
