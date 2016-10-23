8d7ccd1cb77b6716b078af116dcecb1d3b6563db
Anuja Joshi <anuja.joshi@joshsoftware.com>
2016-10-23 13:03:13 +0530

    Added gifts/result page

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,14 @@
 class GiftsController < ApplicationController
-  
+  before_filter :load_category, only: [:index]
+
   def index
+    @gifts = @category.gifts
+    @message = RESULT_MESSAGE.sample
+  end
+
+  private
+
+  def load_category
+    @category = Category.find(params[:category_id])
   end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,8 @@
 FEMALE = "f" 
 MALE = "m" 
-
+RESULT_MESSAGE =
+  [
+    "Yaay, now you have got plenty of options!", "Let's Dig!",
+    "Choose from the irresistible", "Someone is gonna get Lucky!",
+    "Oops... Is this too much?"
+  ]
